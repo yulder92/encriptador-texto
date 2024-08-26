b33f67a25231cc00876f63df5ba59f2387fad425
yulder92 <yulder27@yahoo.es>
2024-08-25 21:22:37 -0500

    Inicio del proyecto

---
+++ js/app.js
@@ -0,0 +1,47 @@
+const textArea=document.querySelector(".text-area");
+const mensaje =document.querySelector(".mensaje");
+
+
+
+// La letra "e" es convertida para "enter"
+// La letra "i" es convertida para "imes"
+// La letra "a" es convertida para "ai"
+// La letra "o" es convertida para "ober"
+// La letra "u" es convertida para "ufat"
+
+let matrizCodigo
+
+Fuction Encriptar(StringEncriptada){
+
+}
+const textEncriptado =encriptar(textArea.value)
+mensaje.value = textEncriptado.textArea
+textArea.value="";
+mensaje.StylebackgroundImage="none"
+
+  console.table (matrizCodigo)=[["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+StringEncriptada=StringEncriptada.tolowerCase()
+
+
+for(let i=0; i <matrizCodigo.length; i++){
+    if (StringEncriptada.includes(matrizCodigo{i}{0})){
+        StringEncriptada=StringEncriptada.ReplaceAll
+
+    }
+}
+return StringEncriptada
+
+Fuction btnDesncriptar(){}
+const textEncriptado =Desencriptar(textArea.value)
+mensaje.value = textdesencriptado
+textArea.value="";
+mensaje.StylebackgroundImage="none"
+
+  console.table (matrizCodigo)=[["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u"."ufat"]];
+  console.table (matrizCodigo)
+
+StringDesncriptada= StringDesencriptada.tolowerCase()
+
+for(let i=0;i<matrizCodigo.length;i++){
+    if (StringEncriptada.includes(matrizCodigo{i}{0})){
+        StringEncriptada=StringDesencriptada.ReplaceAll
